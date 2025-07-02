89b63f2fedc0cb2f3e1aa4e21426838b1cced820
PhilipMuendo <145865457+PhilipMuendo@users.noreply.github.com>
2025-07-02 13:41:52 +0300

    MongoDB connection logic

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/authRoutes.js';
 import cartRoutes from './routes/cartRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import wishlistRoutes from './routes/wishlistRoutes.js';
+import connectDB from './config/db.js';
 
 // Load environment variables
 dotenv.config();
@@ -31,13 +32,8 @@ app.use('/api/wishlist', wishlistRoutes);
 
 // MongoDB connection
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
+connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
